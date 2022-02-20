3df6aff122297b3f7c6f11be09391e783be0b9e6
chaubatkunda <maniklusi31@gmail.com>
2022-02-20 13:08:16 +0700

    querybuilder dan repository category book selesai

--- app/Http/Controllers/Admin/CategoryBookController.php
+++ app/Http/Controllers/Admin/CategoryBookController.php
@@ -3,65 +3,40 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryBookRequest;
 use App\Models\CategoryBook;
+use App\Repositories\CategoryBookRepository;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
+
 
 class CategoryBookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+    private $categoryBookRepository;
+
+    public function __construct(CategoryBookRepository $categoryBookRepository)
+    {
+        $this->categoryBookRepository = $categoryBookRepository; //
+    }
+
     public function index(Request $request)
     {
         return inertia('Category/Index', [
-            'categori_books'    => CategoryBook::when($request->search, function ($query, $search) {
-                $query->where('nama', 'LIKE', '%' . $search . '%');
-            })->paginate(7)
+            'categori_books'    => $this->categoryBookRepository->getAll($request) //mengambil data dari CategoryBookRepository
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return inertia('Category/Create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function store(CategoryBookRequest $request)
     {
-        $request->validate([
-            'nama'  => 'required',
-            'lokasi'  => 'required'
-        ]);
-        CategoryBook::create([
-            'nama'  => $request->nama,
-            'slug'  => Str::slug($request->nama),
-            'lokasi'    => $request->lokasi
-        ]);
-
-        return redirect()
-            ->route('category.index')
-            ->with('success', 'Category Created');
+        $this->categoryBookRepository->storeAll($request); //menerina data yang sudah divalidasi di categoryBookRequest dan mengirim ke categoryBookRepository untuk di simpan di method storeAll
+        return redirect()->route('category.index')->with('success', 'Category Created');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\CategoryBook  $categoryBook
-     * @return \Illuminate\Http\Response
-     */
     public function show(CategoryBook $category)
     {
         return inertia('Category/Show', [
@@ -69,12 +44,6 @@ class CategoryBookController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\CategoryBook  $categoryBook
-     * @return \Illuminate\Http\Response
-     */
     public function edit(CategoryBook $category)
     {
         return inertia('Category/Edit', [
@@ -82,37 +51,12 @@ class CategoryBookController extends Controller
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\CategoryBook  $categoryBook
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, CategoryBook $category)
+    public function update(CategoryBookRequest $request, CategoryBook $category)
     {
-        $request->validate([
-            'nama'  => 'required',
-            'lokasi'  => 'required'
-        ]);
-
-        $category->update([
-            'nama'  => $request->nama,
-            'lokasi'    => $request->lokasi
-        ]);
-
-        return redirect()
-            ->route('category.index')
-            ->with('success', 'Category Updated');
-        // return Redirect::route('category.index')->with('success', 'Category Updated');
+        $this->categoryBookRepository->updateAll($request, $category);
+        return redirect()->route('category.index')->with('success', 'Category Updated');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\CategoryBook  $categoryBook
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(CategoryBook $category)
     {
         $category->delete();
---
+++ app/Repositories/CategoryBookRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\{CategoryBook};
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class CategoryBookRepository
+{
+    public function getAll(Request $request)
+    {
+        $categori_books = DB::table('category_books')
+            ->when($request->search, function ($query, $search) {
+                $query->where('nama', 'LIKE', '%' . $search . '%');
+            })->paginate(7);
+        return $categori_books;
+    }
+
+    /**
+     * Proses menyimpan data
+     */
+    public function storeAll($request)
+    {
+        $categoryBook = new CategoryBook();
+        $categoryBook->nama  = $request->nama;
+        $categoryBook->slug  = Str::slug($request->nama);
+        $categoryBook->lokasi    = $request->lokasi;
+        return $categoryBook->save();
+    }
+
+    /**
+     * Proses update data
+     */
+    public function updateAll($request, $category)
+    {
+        $category->nama  = $request->nama;
+        $category->lokasi = $request->lokasi;
+        return $category->save();
+    }
+}
